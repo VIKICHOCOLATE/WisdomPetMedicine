39990e1ddbee9d7cf240fc8636cb65d6c7c1ba38
vsuponenka <viktoriasuponenko@gmail.com>
2023-10-22 19:59:39 +0200

    Fixed Pet.Domain usings

--- WisdomPetMedicine.Pet.Domain/Events/DomainEvents.cs
+++ WisdomPetMedicine.Pet.Domain/Events/DomainEvents.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WisdomPetMedicine.Common;
+﻿using WisdomPetMedicine.Common;
 
 namespace WisdomPetMedicine.Pet.Domain.Events
 {
--- WisdomPetMedicine.Pet.Domain/Services/FakeBreedService.cs
+++ WisdomPetMedicine.Pet.Domain/Services/FakeBreedService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WisdomPetMedicine.Pet.Domain.ValueObjects;
 
 namespace WisdomPetMedicine.Pet.Domain.Services
 {
