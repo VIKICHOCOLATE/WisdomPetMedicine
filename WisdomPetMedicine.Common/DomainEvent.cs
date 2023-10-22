428610242b85eff9788bbbff445f7fd0b52e7718
vsuponenka <viktoriasuponenko@gmail.com>
2023-10-22 20:33:46 +0200

    Fixed Registermethod

--- WisdomPetMedicine.Common/DomainEvent.cs
+++ WisdomPetMedicine.Common/DomainEvent.cs
@@ -9,14 +9,22 @@ namespace WisdomPetMedicine.Common
     public class DomainEvent<T>
     {
         private List<Action<T>> Actions { get; } = new List<Action<T>>();
-        public void Register()
+        public void Register(Action<T> callback)
         {
+            if (Actions.Exists(a => a.Method == callback.Method))
+            {
+                return;
+            }
 
+            Actions.Add(callback);
         }
 
         public void Publish(T args)
         {
-
+            foreach (Action<T> item in Actions)
+            {
+                item.Invoke(args);
+            }
         }
     }
 }
